518aebf73013a597a09e4bb3ec2a27cf7908977d
fabien <fabienbayon@email.fr>
2020-05-05 08:52:08 +0000

    ajoutcollab

--- web/index.php
+++ web/index.php
@@ -4,6 +4,9 @@ session_start();
 require_once '../src/lib/vendor/autoload.php';
 require_once '../src/config/routing.php';
 require_once '../src/controleur/controleur.php';
+require_once '../src/app/connexion.php';
+require_once '../src/config/parametres.php';
+
 
 $loader = new Twig_Loader_Filesystem('../src/vue/');
 $twig = new Twig_Environment($loader, array());
