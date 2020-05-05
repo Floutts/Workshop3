9fe2abe0e378221b44979338f55107c9c295e08e
fabien <fabienbayon@email.fr>
2020-05-05 09:55:39 +0000

    collaborateur

--- src/controleur/controleur_collabo.php
+++ src/controleur/controleur_collabo.php
@@ -1,7 +1,27 @@
 <?php
 
 function actionAjoutCollabo($twig) {
-    echo $twig->render('ajoutCollabo.html.twig', array());
+    $form = array();
+    if (isset($_POST['btAjouter'])) {
+        $form['valide'] = true;
+        $prenom = $_POST['prenom'];
+        $nom = $_POST['nom'];
+        $email = $_POST['email'];
+        $mdp = $_POST['mdp'];
+        $confMdp =$_POST['confMdp'];
+        $form['nom'] = $nom;
+        $form['prenom'] = $prenom;
+        $form['email'] = $email;
+        $form['mdp'] = $mdp;
+        $form['confMdp'] = $confMdp;
+
+        if ($mdp!=$confMdp){
+            $form['valide'] = false;
+            $form['message'] = 'Les mots de passe sont différents';
+        }
+    }
+
+    echo $twig->render('ajoutCollabo.html.twig', array('form'=>$form));
 }
 function actionListeCollabo($twig) {
     echo $twig->render('listeCollabo.html.twig', array());
