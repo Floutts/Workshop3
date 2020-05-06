5613b25299a9a60fd6ec2fa183996bd2d3042fb3
fabien <fabienbayon@email.fr>
2020-05-06 09:29:53 +0000

    ajoutToutSaufOption

--- src/controleur/controleur_salle.php
+++ src/controleur/controleur_salle.php
@@ -1,5 +1,19 @@
 <?php
 
 function actionGestionSalle($twig) {
-    echo $twig->render('gestionSalle.html.twig', array());
+    $form = array();
+    if (isset($_POST['btAjouter'])) {
+        $form['valide'] = true;
+        $nom = $_POST['nom'];
+        $prix = $_POST['prix'];
+        $superficie = $_POST['superficie'];
+
+        $form['nom'] = $nom;
+        $form['prix'] = $prix;
+        $form['superficie'] = $superficie;
+
+
+    }
+
+    echo $twig->render('gestionSalle.html.twig', array('form'=>$form));
 }
