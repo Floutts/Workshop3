c04b7d29727bc18b477c0fb065d17de19e9fdc63
Maxence <maziere.maxence@gmail.com>
2020-05-14 09:56:20 +0000

    reservation

--- src/controleur/controleur_reserver.php
+++ src/controleur/controleur_reserver.php
@@ -4,6 +4,13 @@ function actionTableReservation($twig) {
     echo $twig->render('tableReservation.html.twig', array());
 }
 
-function actionReserver($twig) {
-    echo $twig->render('reserver.html.twig', array());
+function actionReserver($twig,$db) {
+    $form = array();
+    $association = new Association($db);
+    $salle = new Salle($db);
+    $option = new Option($db);
+    $listeAssociation = $association->select();
+    $listeSalle = $salle->select();
+    $listeOption = $option->select();
+    echo $twig->render('reserver.html.twig', array('listeAssociation'=>$listeAssociation,'listeOption'=>$listeOption,'listeSalle'=>$listeSalle));
 }
