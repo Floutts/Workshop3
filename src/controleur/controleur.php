5dee5d171504d5f01660e798bb9118d2b863a3b3
Maxence <maxence.maziere@epsi.fr>
2021-10-05 10:03:17 +0200

    base

--- src/controleur/controleur.php
+++ src/controleur/controleur.php
@@ -1,7 +1,3 @@
 <?php
-require_once 'controleur_association.php';
-require_once 'controleur_option.php';
-require_once 'controleur_reserver.php';
-require_once 'controleur_salle.php';
-require_once 'controleur.php';
+
 require_once 'controleur_index.php';
--- src/modele/_classes.php
+++ src/modele/_classes.php
@@ -1,7 +1,1 @@
 <?php
-require_once 'class_option.php';
-require_once 'class_salle.php';
-require_once 'class_association.php';
-require_once 'class_utilisateurs.php';
-require_once 'class_reserver.php';
-require_once 'class_date.php';
